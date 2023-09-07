8b39c22afa246f567d44707edd6fa5a7d6f00579
bkj05 <141669589+bkj05@users.noreply.github.com>
2023-09-07 14:11:18 -0700

    Finished project

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from weather import main as get_weather  # assuming this is a module you've written
+import requests  # add this line for making external HTTP requests
+import os  # add this line for environment variables
+
+app = Flask(__name__)
+API_KEY = os.getenv('API_KEY')  # assuming you have set an environment variable named 'API_KEY'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather_data = None
+    if request.method == 'POST':
+        city = request.form['cityName']
+        state = request.form['stateName']
+        country = request.form['countryName']
+        # Change the following line
+        response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid={API_KEY}&units=metric').json()
+        
+        if response.get('cod') == 200:
+            weather_data = {
+                'main': response['weather'][0]['main'],
+                'description': response['weather'][0]['description'],
+                'icon': response['weather'][0]['icon'],
+                'temperature': response['main']['temp'],
+                'humidity': response['main']['humidity'],
+                'pressure': response['main']['pressure'],
+                'wind_speed': response['wind']['speed']
+            }
+    return render_template('index.html', weather_data=weather_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- weather.py
+++ weather.py
@@ -1,23 +1,47 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+from dotenv import load_dotenv
+import os
+from dataclasses import dataclass
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+
+@dataclass
+class WeatherData:
+    main: str
+    description: str
+    icon: str
+    temperature: float
+
+def get_lat_lon(city_name, state_code, country_code, API_key):
+    response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
+    data = response[0]
+    lat, lon = data.get('lat'), data.get('lon')
+    return lat, lon
+
+def get_current_weather(lat, lon, API_key):
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=metric').json()
+    data = WeatherData(
+        main=response.get('weather')[0].get('main'),
+        description=response.get('weather')[0].get('description'),
+        icon=response.get('weather')[0].get('icon'),
+        temperature=response.get('main').get('temp')
+    )
+    return data
+
 app = Flask(__name__)
-# import request
-# from dotenv import load_dotenv
-# import os
 
-# load_dotenv()
-# API_KEY = os.getenv ('API_KEY')
-
-@app.route("/")
-def hello():
-   return "Hello World!"
-
-#def get_lan_lon(city_name, state_code, country_code, API_KEY):
-    #response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_KEY}')
-
-    #print(response)
-    #print('Hello World')
-#get_lan_lon('Toronto', 'ON', 'Canada', API_KEY)
-
+@app.route("/weather/<city_name>/<state_code>/<country_code>", methods=["GET"])
+def main(city_name, state_code, country_code):
+    lat, lon = get_lat_lon(city_name, state_code, country_code, api_key)
+    weather_data = get_current_weather(lat, lon, api_key)
+    return jsonify(
+        main=weather_data.main,
+        description=weather_data.description,
+        icon=weather_data.icon,
+        temperature=weather_data.temperature
+    )
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0')
+    app.run(debug=True)
